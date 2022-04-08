545e100df58c7d8254dea64d91d823ff0bbd8d61
Aleesharyan <1027437@student.sae.edu.au>
2022-04-08 12:55:56 +1000

    Initial commit

---
+++ Assets/Blackjack.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blackjack : MonoBehaviour
+{
+
+    //variable to store our current roll
+    int myTotal = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // listen for the 'h' key to be pressed 
+
+        if(Input.GetKeyDown("h"))
+        {
+            //explain to the player what has happened
+            print("Hit! Time to roll!");
+
+            //call the hit function and store the return value in 'myTotal'
+            myTotal = Hit();
+            string printString;
+
+            //explain to player what has happened, check for win value
+            printString = ("You rolled a " + myTotal + "\n");
+            if(myTotal == 6)
+            {
+                printString += ("You win!");
+                //tell particle system to play its particle animation
+                
+            }
+            else
+            {
+                printString += ("You lose");
+            }
+            print(printString);
+
+            //send printString to UI object
+            //myUI.UpdateTheText(printString);
+            //myParticle.PlayParticle();
+
+            EventManager.GameWin(printString);
+        }
+
+
+    }
+
+    //function to determine the value of a roll
+    int Hit()
+    {
+        //genterate a random number between 1-6
+        int returnValue = Random.Range(1,7);
+
+        //gives us a random number
+        return returnValue;
+    }
+
+}
